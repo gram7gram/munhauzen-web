53f5629a6304eecc7aafd2a09fed5f8b3ff57f7a
Gram <gram7gram@gmail.com>
2019-05-24 16:19:20 +0300

    #master hotfix

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 
-git config -g user.email gram7gram@gmail.com && git config -g user.name Gram
+git config --global user.email gram7gram@gmail.com && git config --global user.name Gram
 
 git checkout -b $1
 
 git pull origin $1
 
-cd www
-
-./npm run build:prod
+cd www && /npm run build:prod
