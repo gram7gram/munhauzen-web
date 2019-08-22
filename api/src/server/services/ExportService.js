99f5f9a6cd21ac75d1cfe7874c738639b98e7a1c
Gram <gram7gram@gmail.com>
2019-08-22 23:49:11 +0300

    #master hotfix

--- api/src/server/services/ExportService.js
+++ api/src/server/services/ExportService.js
@@ -21,7 +21,7 @@ const ExportService = (function () {
   Service.prototype.generateArchive = async function () {
 
     const archive = archiver('zip', {
-      zlib: {level: 9}
+      zlib: {level: 5}
     });
 
     archive.on('error', function (err) {
@@ -31,58 +31,96 @@ const ExportService = (function () {
       throw err;
     })
 
-    archive.append(await this.prepareAudio(), {name: 'audio.json'})
-    archive.append(await this.prepareAudioFails(), {name: 'audio-fails.json'})
-    archive.append(await this.prepareChapters(), {name: 'chapters.json'})
-    archive.append(await this.prepareScenario(), {name: 'scenario.json'})
-    archive.append(await this.prepareInventory(), {name: 'inventory.json'})
-    archive.append(await this.prepareImages(), {name: 'images.json'})
+    const audio = await this.prepareAudio()
+    if (audio.length === 0) {
+      throw 'Missing audio'
+    }
 
-    const fileName = '/downloads/game.zip'
+    const fails = await this.prepareAudioFails()
+    if (fails.length === 0) {
+      throw 'Missing fails'
+    }
+
+    const chapters = await this.prepareChapters()
+    if (chapters.length === 0) {
+      throw 'Missing chapters'
+    }
+
+    const scenario = await this.prepareScenario()
+    if (scenario.length === 0) {
+      throw 'Missing scenario'
+    }
+
+    const inventory = await this.prepareInventory()
+    if (inventory.length === 0) {
+      throw 'Missing inventory'
+    }
+
+    const images = await this.prepareImages()
+    if (images.length === 0) {
+      throw 'Missing images'
+    }
+
+    archive.append(JSON.stringify(audio), {name: 'audio.json'})
+    archive.append(JSON.stringify(fails), {name: 'audio-fails.json'})
+    archive.append(JSON.stringify(chapters), {name: 'chapters.json'})
+    archive.append(JSON.stringify(scenario), {name: 'scenario.json'})
+    archive.append(JSON.stringify(inventory), {name: 'inventory.json'})
+    archive.append(JSON.stringify(images), {name: 'images.json'})
+
+    const fileName = `/downloads/game-${parameters.version}.zip`
 
     archive.pipe(fs.createWriteStream(publicDir + fileName));
 
-    archive.finalize();
+    await archive.finalize();
 
     return {
-      url: parameters.host + fileName
+      url: parameters.host + fileName,
+      summary: {
+        audio: audio.length,
+        fails: fails.length,
+        chapters: chapters.length,
+        scenario: scenario.length,
+        inventory: inventory.length,
+        images: images.length,
+      }
     }
   }
 
   Service.prototype.prepareAudioFails = async function () {
     const items = await AudioFail.find().sort({name: 'asc'}).lean()
 
-    return JSON.stringify(items)
+    return items
   }
 
   Service.prototype.prepareChapters = async function () {
     const items = await Chapter.find().sort({name: 'asc'}).lean()
 
-    return JSON.stringify(items)
+    return items
   }
 
   Service.prototype.prepareScenario = async function () {
     const items = await Scenario.find({isReserved: {$ne: true}}).sort({name: 'asc'}).lean()
 
-    return JSON.stringify(items)
+    return items
   }
 
   Service.prototype.prepareInventory = async function () {
     const items = await Inventory.find().sort({name: 'asc'}).lean()
 
-    return JSON.stringify(items)
+    return items
   }
 
   Service.prototype.prepareImages = async function () {
     const items = await Image.find({isReserved: {$ne: true}}).sort({order: 'asc', name: 'asc'}).lean()
 
-    return JSON.stringify(items)
+    return items
   }
 
   Service.prototype.prepareAudio = async function () {
     const items = await Audio.find().sort({name: 'asc'}).lean()
 
-    return JSON.stringify(items)
+    return items
   }
 
   return new Service();
