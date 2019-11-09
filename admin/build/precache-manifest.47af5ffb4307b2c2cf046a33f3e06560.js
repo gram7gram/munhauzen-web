44ff24228edbaa778e9909d85d0daadff346cc95
Gram <gram7gram@gmail.com>
2019-11-09 18:05:01 +0200

    #master fixed

---
+++ admin/build/precache-manifest.47af5ffb4307b2c2cf046a33f3e06560.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd5f5bd850f7aa6d3d1394ee489376d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4dd01189fbc59d9f816",
+    "url": "/static/css/2.6831b910.chunk.css"
+  },
+  {
+    "revision": "370ef7b0d221f80193ab",
+    "url": "/static/css/main.c57baf1c.chunk.css"
+  },
+  {
+    "revision": "a4dd01189fbc59d9f816",
+    "url": "/static/js/2.3c76fcc3.chunk.js"
+  },
+  {
+    "revision": "370ef7b0d221f80193ab",
+    "url": "/static/js/main.587b7e5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "39bb198735c6eb2a9ee468cb33f5f7a7",
+    "url": "/static/media/BuxtonSketch.39bb1987.ttf"
+  },
+  {
+    "revision": "066f194583f83f1301ac202ee98da706",
+    "url": "/static/media/app-store.066f1945.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "353a17a971ff7015b1cf85595b2eab0a",
+    "url": "/static/media/loading_ground.353a17a9.png"
+  },
+  {
+    "revision": "65bbb6c94d6920bc08a8a98c47c11282",
+    "url": "/static/media/menu_logo.65bbb6c9.png"
+  },
+  {
+    "revision": "b9511fd7107d10b9d4c0d0a57ad1b3c8",
+    "url": "/static/media/p0.b9511fd7.jpg"
+  },
+  {
+    "revision": "0f8fe0415cec594559a4c4c376135fe9",
+    "url": "/static/media/Жене.0f8fe041.jpg"
+  },
+  {
+    "revision": "3d88bdd1714bbe1d3452498d84c4f813",
+    "url": "/static/media/Космос.3d88bdd1.jpg"
+  },
+  {
+    "revision": "d9f0df8df04b813db2a2c4069382763e",
+    "url": "/static/media/Лопата_Рішуче.d9f0df8d.jpg"
+  }
+]);
